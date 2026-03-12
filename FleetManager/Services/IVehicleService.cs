3e4c9441de0326d27c209a786f5a2f7cb003c55b
jakub <jakub.jaworowski@cyfroweszkoly.pl>
2026-03-12 11:11:55 +0100

    Json files handling

--- FleetManager/Services/IVehicleService.cs
+++ FleetManager/Services/IVehicleService.cs
@@ -1,6 +1,11 @@
-namespace FleetManager.Services;
-
-public interface IVehicleService
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FleetManager.Models;
+namespace FleetManager.Services
 {
-    
+    public interface IVehicleService
+    {
+        Task<IEnumerable<Vehicle>> LoadVehiclesAsync();
+        Task SaveVehiclesAsync(IEnumerable<Vehicle> vehicles);
+    }
 }
--- FleetManager/Services/JsonVehicleService.cs
+++ FleetManager/Services/JsonVehicleService.cs
@@ -1,6 +1,34 @@
-namespace FleetManager.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using FleetManager.Models;
+using FleetManager.Services;
 
-public class JsonVehicleService
+namespace SwiftTransFleet.Services
 {
-    
+    public class JsonVehicleService : IVehicleService
+    {
+        
+        private readonly string _filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "vehicles.json");
+
+        public async Task<IEnumerable<Vehicle>> LoadVehiclesAsync()
+        {
+            if (!File.Exists(_filePath))
+            {
+                return new List<Vehicle>(); 
+            }
+
+            using FileStream openStream = File.OpenRead(_filePath);
+            return await JsonSerializer.DeserializeAsync<List<Vehicle>>(openStream) ?? new List<Vehicle>();
+        }
+
+        public async Task SaveVehiclesAsync(IEnumerable<Vehicle> vehicles)
+        {
+            var options = new JsonSerializerOptions { WriteIndented = true }; 
+            using FileStream createStream = File.Create(_filePath);
+            await JsonSerializer.SerializeAsync(createStream, vehicles, options);
+        }
+    }
 }
